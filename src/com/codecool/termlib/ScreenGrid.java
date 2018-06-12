9338e73bf2f3f3c74d118bf228ab3d699b60c6ab
timardaniel <daniel.timar21@gmail.com>
2018-06-12 13:38:22 +0200

    Obstacle place

--- src/com/codecool/termlib/ScreenGrid.java
+++ src/com/codecool/termlib/ScreenGrid.java
@@ -13,17 +13,25 @@ public class ScreenGrid {
     
     public void refreshGrid(){
         Dino dino = new Dino();
-        
-        
         String[][] dinoShape = dino.getShape();
         int dinoX = dino.getXPosition();
         int dinoY = dino.getYPosition();
-        for (int i = 0; i<dinoShape.length; i++){
-            for (int j = 0; j<dinoShape[i].length; j++){
+        for (int i = 0; i<dinoShape.length && i+dinoY < 40; i++){
+            for (int j = 0; j<dinoShape[i].length && j+dinoX < 150; j++){
                 this.grid[i+dinoY][j+dinoX] = dinoShape[i][j];
             }
         }
-
+        
+        
+        Obstacle obstacle = new Obstacle();
+        String[][] obstacleShape = obstacle.getShape();
+        int obstacleX = obstacle.getXPosition();
+        int obstacleY = obstacle.getYPosition();
+        for (int i = 0; i < obstacleShape.length && i+obstacleY < 40; i++){
+            for(int j = 0; j < obstacleShape.length && j+obstacleX < 150; j++){
+                this.grid[i+obstacleY][j+obstacleX] = obstacleShape[i][j];
+            }
+        }
     }
 
     public String[][] getGrid(){
